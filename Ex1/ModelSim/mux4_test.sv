db5247e893537b9b0967dd84cc15f1a94949a800
Boaz Montilya <boaz2.montilya@gmail.com>
2023-06-02 15:02:53 +0300

    changed stuff according to new orders

--- Ex1/ModelSim/mux4_test.sv
+++ Ex1/ModelSim/mux4_test.sv
@@ -7,18 +7,26 @@ logic [1:0] sel;
 mux4 uut (.d0(d0), .d1(d1), .d2(d2), .d3(d3), .z(z), .sel(sel));
 
 initial begin
-    {d0,d1,d2,d3} = 4'd2;
+    {d0,d1,d2,d3} = 4'b1000;
     sel = 2'b00;
 
-    #30
+    #40
 
     sel = 2'b10;
 
-    #15
+    #20
     
     sel = 2'b00;
 
-    #15
+    #20
+
+    sel = 2'b01;
+
+    #40
+
+    sel = 2'b00;
+
+    #40
 
     $stop;
 end
